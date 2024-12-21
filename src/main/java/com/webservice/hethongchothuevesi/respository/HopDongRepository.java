fa9bdaf343bd77161d6c663af64197ccafcb0f5e
Nguyễn Xuân Huỳnh <xuanhuynh543@gmail.com>
2024-12-21 16:22:54 +0700

    Truy vấn thêm tên danh mục ở hợp đồng

--- src/main/java/com/webservice/hethongchothuevesi/respository/HopDongRepository.java
+++ src/main/java/com/webservice/hethongchothuevesi/respository/HopDongRepository.java
@@ -1,8 +1,6 @@
 package com.webservice.hethongchothuevesi.respository;
 
-import java.util.List;
-import java.util.Optional;
-
+import com.webservice.hethongchothuevesi.dto.dto.HopDongDTO;
 import com.webservice.hethongchothuevesi.entity.HopDong;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,24 +9,52 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface HopDongRepository extends JpaRepository<HopDong, Integer> {
-	List<HopDong> findByNgayXoaIsNull();
+    List<HopDong> findByNgayXoaIsNull();
 
-	Optional<HopDong> findByIdHopDong(int idHopDong);
+    Optional<HopDong> findByIdHopDong(int idHopDong);
 
-	Optional<HopDong> findByIdHopDongAndNgayXoaIsNull(int idHopDong);
+    Optional<HopDong> findByIdHopDongAndNgayXoaIsNull(int idHopDong);
 
-	long countByNgayXoaIsNull();
+    @Query("SELECT new com.webservice.hethongchothuevesi.dto.dto.HopDongDTO(" +
+            "h.idHopDong, h.idYeuCauDichVu, d.tenDichVu, h.hoTenKhachHang, h.soDienThoai, h.anh, h.soCccd, " +
+            "h.diaDiem, h.soLuongVeSi, h.thoiGianBatDau, h.thoiGianKetThuc, h.tongChiPhi, h.ghiChu, " +
+            "h.soSaoDanhGia, h.thongTinDanhGia, h.trangThai, h.ngayXoa) " +
+            "FROM HopDong h " +
+            "JOIN YeuCauDichVu y ON h.idYeuCauDichVu = y.idYeuCauDichVu " +
+            "JOIN DanhMucDichVu d ON y.idDanhMucDichVu = d.idDanhMucDichVu " +
+            "WHERE h.idHopDong = :idHopDong AND h.ngayXoa IS NULL")
+    HopDongDTO findHopDongWithTenDichVuByIdHopDongAndNgayXoaIsNull(@Param("idHopDong") int idHopDong);
 
-	Page<HopDong> findByNgayXoaIsNull(Pageable pageable);
+    long countByNgayXoaIsNull();
 
-	@Query(
-			value = "SELECT h.* " + "FROM HopDong h "
-					+ "JOIN YeuCauDichVu y ON h.idYeuCauDichVu = y.idYeuCauDichVu "
-					+ "WHERE y.idNguoiDung = :idNguoiDung "
-					+ "AND h.ngayXoa IS NULL "
-					+ "AND y.ngayXoa IS NULL",
-			nativeQuery = true)
-	List<HopDong> findAllHopDongByIdNguoiDungAndNgayXoaIsNull(@Param("idNguoiDung") Integer idNguoiDung);
+    Page<HopDong> findByNgayXoaIsNull(Pageable pageable);
+
+    @Query("SELECT new com.webservice.hethongchothuevesi.dto.dto.HopDongDTO(" +
+            "h.idHopDong, h.idYeuCauDichVu, d.tenDichVu, h.hoTenKhachHang, h.soDienThoai, h.anh, h.soCccd, " +
+            "h.diaDiem, h.soLuongVeSi, h.thoiGianBatDau, h.thoiGianKetThuc, h.tongChiPhi, h.ghiChu, " +
+            "h.soSaoDanhGia, h.thongTinDanhGia, h.trangThai, h.ngayXoa) " +
+            "FROM HopDong h " +
+            "JOIN YeuCauDichVu y ON h.idYeuCauDichVu = y.idYeuCauDichVu " +
+            "JOIN DanhMucDichVu d ON y.idDanhMucDichVu = d.idDanhMucDichVu " +
+            "WHERE y.idNguoiDung = :idNguoiDung " +
+            "AND h.ngayXoa IS NULL " +
+            "AND y.ngayXoa IS NULL")
+    List<HopDongDTO> findAllHopDongWithTenDichVuByIdNguoiDungAndNgayXoaIsNull(@Param("idNguoiDung") Integer idNguoiDung);
+
+
+    @Query("SELECT new com.webservice.hethongchothuevesi.dto.dto.HopDongDTO(" +
+            "h.idHopDong, h.idYeuCauDichVu, d.tenDichVu, h.hoTenKhachHang, h.soDienThoai, h.anh, h.soCccd, " +
+            "h.diaDiem, h.soLuongVeSi, h.thoiGianBatDau, h.thoiGianKetThuc, h.tongChiPhi, h.ghiChu, " +
+            "h.soSaoDanhGia, h.thongTinDanhGia, h.trangThai, h.ngayXoa) " +
+            "FROM HopDong h " +
+            "JOIN YeuCauDichVu y ON h.idYeuCauDichVu = y.idYeuCauDichVu " +
+            "JOIN DanhMucDichVu d ON y.idDanhMucDichVu = d.idDanhMucDichVu " +
+            "WHERE h.ngayXoa IS NULL")
+    List<HopDongDTO> findAllHopDongWithTenDichVuAndNgayXoaIsNull();
+
 }
